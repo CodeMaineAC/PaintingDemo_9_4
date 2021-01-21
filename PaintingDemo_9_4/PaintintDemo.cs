fecfd4e4a3838414438a5d701c73e41d2a440c46
Benjamin Goff <bgoff@mainerobotics.org>
2021-01-21 10:58:40 -0500

    Exercise 9-4 with a concole based UI that gets the room dimensions from the user

--- PaintingDemo_9_4/PaintintDemo.cs
+++ PaintingDemo_9_4/PaintintDemo.cs
@@ -7,39 +7,13 @@ namespace PaintingDemo_9_4
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            Room[] rooms = new Room[8];
+            UserInt ui = new UserInt();
 
-            int length, width, height;
-
-            int x;
-            for (x = 0; x < rooms.Length; ++x)
-            {
-                length = rand.Next(5, 25);
-                width = rand.Next(5, 25);
-                height = rand.Next(6, 12);
-
-                rooms[x] = new Room(length, width, height);
-            }
-
-            int roomNumber;
-            for(x = 0; x < rooms.Length; ++x)
-            {
-                roomNumber = x + 1;
-                RoomProperties(roomNumber, rooms[x]);
-            }
-            
+            ui.GetRoomDim();
+            ui.PrintRoomProperties();   
         }
 
-        static void RoomProperties(int roomNumber, Room room)
-        {
-            Console.WriteLine("Room Number {0}", roomNumber);
-            Console.WriteLine("{0}L X {1}W X {2}H",
-                room.Length, room.Width, room.Height);
-            Console.WriteLine("Room area: {0} SqrFt\nGallons of paint used: {1}"
-                , room.Area, room.Gallons);
-            Console.WriteLine("");
-        }
+        
     }
 
 
---
+++ PaintingDemo_9_4/UserInt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaintingDemo_9_4
+{
+    class UserInt
+    {
+        List<Room> rooms = new List<Room>();
+
+            
+
+        //Prompt the user for dimensions of the room
+        public void GetRoomDim()
+        {
+            string uInput = "";
+            string[] LWH;
+            int length, width, height;
+            
+            while(true)
+            {
+                Console.WriteLine("List the room Length, Width, " +
+                "and Height seperated by spaces");
+                uInput = Console.ReadLine();
+                if(uInput.Equals("Q"))
+                {
+                    break;
+                }
+                LWH = uInput.Split(' ');
+
+                length = Convert.ToInt32(LWH[0]);
+                width = Convert.ToInt32(LWH[1]);
+                height = Convert.ToInt32(LWH[2]);
+
+                Room room = new Room(length,width,height);
+                rooms.Add(room);
+
+                Console.WriteLine("Enter 'Q' to stop entering rooms else:");
+
+            }
+
+        }
+
+        public void PrintRoomProperties()
+        {
+            Room room;
+            int roomNumber;
+            int x;
+
+            for(x = 0; x < rooms.Count; ++x)
+            {
+                room = rooms[x];
+                roomNumber = x + 1;
+                Console.WriteLine("Room Number {0}", roomNumber);
+                Console.WriteLine("{0}L X {1}W X {2}H",
+                    room.Length, room.Width, room.Height);
+                Console.WriteLine("Room area: {0} SqrFt\nGallons of paint used: {1}"
+                    , room.Area, room.Gallons);
+                Console.WriteLine("");
+            }
+            
+        }
+    }
+}
