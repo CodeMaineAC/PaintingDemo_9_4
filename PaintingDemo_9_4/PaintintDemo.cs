cf460aa4b968e020a93cb179e8909be10ad28311
Benjamin Goff <bgoff@mainerobotics.org>
2021-01-21 09:48:39 -0500

    Add project files.

---
+++ PaintingDemo_9_4/PaintintDemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace PaintingDemo_9_4
+{
+    class PaintintDemo
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Room[] rooms = new Room[8];
+
+            int length, width, height;
+
+            int x;
+            for (x = 0; x < rooms.Length; ++x)
+            {
+                length = rand.Next(5, 25);
+                width = rand.Next(5, 25);
+                height = rand.Next(6, 12);
+
+                rooms[x] = new Room(length, width, height);
+            }
+
+            int roomNumber;
+            for(x = 0; x < rooms.Length; ++x)
+            {
+                roomNumber = x + 1;
+                RoomProperties(roomNumber, rooms[x]);
+            }
+            
+        }
+
+        static void RoomProperties(int roomNumber, Room room)
+        {
+            Console.WriteLine("Room Number {0}", roomNumber);
+            Console.WriteLine("{0}L X {1}W X {2}H",
+                room.Length, room.Width, room.Height);
+            Console.WriteLine("Room area: {0} SqrFt\nGallons of paint used: {1}"
+                , room.Area, room.Gallons);
+            Console.WriteLine("");
+        }
+    }
+
+
+}
---
+++ PaintingDemo_9_4/Room.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaintingDemo_9_4
+{
+    class Room
+    {
+        int length, width, height, area, gallons;
+
+        public Room(int l, int w, int h)
+        {
+            length = l;
+            width = w;
+            height = h;
+            area = FindArea();
+            gallons = FindGallons();
+        }
+        public int Length
+        { 
+            get { return length;}
+        }
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+
+        public int Area
+        {
+            get { return area; }
+        }
+
+        public int Gallons
+        {
+            get { return gallons; }
+        }
+
+        private int FindArea()
+        {
+            return area = length * height * 2 + width * height * 2;
+        }
+
+        private int FindGallons()
+        {
+            int gallonsSqrFt = 350;
+            int g = area / gallonsSqrFt;
+            
+            if(area % gallonsSqrFt > 0)
+            {
+                ++g;
+            }
+
+            return g;
+        }
+
+
+
+
+    }
+}
